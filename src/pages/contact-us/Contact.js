f9f4df2f361fa615e4c93d729c2b4328121cd5d0
Ghulame Mustafa <108045115+ghulame22@users.noreply.github.com>
2024-03-27 13:39:39 +0530

    Minor fix

--- src/pages/contact-us/Contact.js
+++ src/pages/contact-us/Contact.js
@@ -1,180 +1,76 @@
-import React from 'react'
-import { Carousel, PageWIthNavbar, Products, Services } from '../../component'
-import Lottie from 'lottie-react';
+import React from 'react';
+import { PageWIthNavbar } from '../../component';
 import { Home1 } from '../../assets';
-import { Link } from 'react-router-dom';
 import { Footer } from '../../component/footer';
 
+const branches = [
+    {
+        name: "HEAD OFFICE ANKLESHWAR",
+        contact: "Sanjay Singh Shekhawat",
+        phone1: "(+91) 91730 92721",
+        phone2: "(+91) 93775 21201",
+        address: "F-27. James Plaza Square, Asian Paints Chokdi, G.I.D.C. Estate, Ankleshwar - 393 002"
+    },
+    {
+        name: "LUDHIANA BRANCH",
+        contact: "Rajveer Singh",
+        phone1: "(+91) 77173 14313",
+        address: "B-23/2818R transport nagar, Opposite plot no 112 back side Goodwill Dharm kanda, Ludhiana 141003"
+    },
+    {
+        name: "SURAT BRANCH",
+        contact: "Vikram Singh",
+        phone1: "(+91) 91730 92721",
+        address: "203, Soham parking, Near Mcdonald's Restaurant NH.8, Karodara, Surat."
+    },
+    {
+        name: "MUMBAI BRANCH",
+        contact: "Omprakash Singh",
+        phone1: "(+91) 63782 21104",
+        address: "Hemu Plaza, 1st floor, Station Road Vile Parle, Mumbai - 400 056"
+    },
+    {
+        name: "JAIPUR BRANCH",
+        contact: "Bharat Singh",
+        phone1: "(+91) 93775 21201",
+        address: "A-611 Opposite of Udyog bhawan, Tilak marg, C- scheme, Jaipur -302 005"
+    }
+];
 
 const Contact = () => {
     return (
         <PageWIthNavbar>
             <div className="flex flex-col items-center justify-center">
                 <div className="flex flex-col w-[80%] mt-4 mb-16 py-4 justify-between gap-y-4">
-                    <span className="text-2xl text-cyan-800">
-                        Contact Us
-                    </span>
+                    <span className="text-2xl text-cyan-800">Contact Us</span>
                     <div className='flex gap-10 flex-wrap'>
-                        <div className='flex'>
-                            <div className='flex flex-col gap-y-2'>
-                                <div>
-                                    <span>
-                                        HEAD OFFICE
-                                    </span>
-                                </div>
-                                <div className='flex flex-col gap-y-2 text-sm'>
-                                    <div>
-                                        <span>
-                                            Contact : Sanjay Singh Shekhawat
-                                        </span>
+                        {branches.map(branch => (
+                            <div key={branch.name}>
+                                <div className='flex flex-col gap-y-2'>
+                                    <div className='flex flex-col'>
+                                        <span>{branch.name}</span>
                                     </div>
-                                    <div>
+                                    <div className='flex flex-col gap-y-2 text-sm'>
                                         <div>
-                                            <span>
-                                                (+91) 91730 92721
-                                            </span>
+                                            <span>Contact: {branch.contact}</span>
                                         </div>
                                         <div>
-                                            <span>
-                                                (+91) 93775 21201
-                                            </span>
+                                            <span>{branch.phone1}</span>
+                                            {branch.phone2 && <span>{branch.phone2}</span>}
                                         </div>
-                                    </div>
-                                    <div className='w-[20rem]'>
-                                        <span>
-                                            F-27. James Plaza Square,
-                                            Asian Paints Chokdi,
-                                            G.I.D.C. Estate,
-                                            Ankleshwar - 393 002
-                                        </span>
+                                        <div className='w-[20rem]'>
+                                            <span>{branch.address}</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div>
-                            <div className='flex flex-col gap-y-2'>
-                                <div>
-                                    <span>
-                                        OUR BRANCH
-                                    </span>
-                                </div>
-                                <div className='flex flex-col gap-y-2 text-sm'>
-                                    <div>
-                                        <span>
-                                            Contact : Rajveer Singh
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <div>
-                                            <span>
-                                                (+91) 77173 14313
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className='w-[20rem]'>
-                                        <span>
-                                            B-23/2818R transport nagar
-                                            Opp plot no 112 back side Goodwill
-                                            Dharm kanda Ludhiana 141003
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='flex flex-col gap-y-2'>
-                                <div>
-                                    <span>
-                                        OUR BRANCH
-                                    </span>
-                                </div>
-                                <div className='flex flex-col gap-y-2 text-sm'>
-                                    <div>
-                                        <span>
-                                            Contact : Vikram Singh
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <div>
-                                            <span>
-                                                (+91) 91730 92721
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className='w-[20rem]'>
-                                        <span>
-                                            203, Soham parking mekdonaldes restorent ke pass NH.8, Karodara, Surat.
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='flex flex-col gap-y-2'>
-                                <div>
-                                    <span>
-                                        OUR BRANCH
-                                    </span>
-                                </div>
-                                <div className='flex flex-col gap-y-2 text-sm'>
-                                    <div>
-                                        <span>
-                                            Contact : Omprakash Singh
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <div>
-                                            <span>
-                                                (+91) 63782 21104
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className='w-[20rem]'>
-                                        <span>
-                                            Hemu Plaza, 1st floor,
-                                            Station Road Vile Parle,
-                                            Mumbai - 400 056
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='flex flex-col gap-y-2'>
-                                <div>
-                                    <span>
-                                        OUR BRANCH
-                                    </span>
-                                </div>
-                                <div className='flex flex-col gap-y-2 text-sm'>
-                                    <div>
-                                        <span>
-                                            Contact : Bharat Singh
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <div>
-                                            <span>
-                                                (+91) 93775 21201
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className='w-[20rem]'>
-                                        <span>
-                                            A-611 Opposit of Udyog bhawan,
-                                            tilak marg, C- scheme,
-                                            Jaipur -302 005
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
             <Footer />
         </PageWIthNavbar>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
